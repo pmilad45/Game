2203e4497bc9479bc89fb1ce5250aa46dc27d50c
DaLotfy31-GUC <you.lotfy@icloud.com>
2026-03-09 01:22:26 +0200

    Added ContaminationSock class (thus my part is done)

--- src/game/engine/cells/ContaminationSock.java
+++ src/game/engine/cells/ContaminationSock.java
@@ -2,13 +2,17 @@ package game.engine.cells;
 
 import game.engine.interfaces.CanisterModifier;
 import game.engine.monsters.*;
+import game.engine.Constants;
 
 public class ContaminationSock extends TransportCell implements CanisterModifier{
+    
     public ContaminationSock(String name, int effect){
         super(name, effect);
     }
 
     @Override
-    public void modifyEnergy()
+    public void modifyEnergy(Monster monster){
+        monster.setEnergy(monster.getEnergy()-Constants.SLIP_PENALTY);
+    }
 
 }
