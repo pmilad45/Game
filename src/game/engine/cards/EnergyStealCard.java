5c7f1034addfb591b161ff35a117d13aa9a9c534
Peter Milad Fawzy <pmilad45@gmail.com>
2026-03-12 13:42:07 +0200

    5.13-6.5

---
+++ src/game/engine/cards/EnergyStealCard.java
@@ -0,0 +1,19 @@
+package game.engine.cards;
+
+public class EnergyStealCard extends Card {
+ private int energy;
+ private static final boolean lucky= true;
+ 
+ EnergyStealCard(String name, String description, int rarity, int energy){
+	 super (name,description,rarity,lucky);
+	 this.energy = energy;
+	 
+	 
+	  
+	 
+ }
+
+public int getEnergy() {
+	return energy;
+}
+}
---
+++ src/game/engine/cards/StartOverCard.java
@@ -0,0 +1,12 @@
+package game.engine.cards;
+
+public class StartOverCard extends Card{
+	StartOverCard(String name, String description, int rarity, boolean lucky){
+		super(name,description,rarity,lucky); 
+	}
+	
+	
+	
+	
+
+}
