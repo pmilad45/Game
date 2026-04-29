299f6cffd3c021e8615bbddabd958280ad676ae8
Peter Milad Fawzy <pmilad45@gmail.com>
2026-04-29 17:22:34 +0300

    fixes in Board.java

--- src/game/engine/Board.java
+++ src/game/engine/Board.java
@@ -32,70 +32,39 @@ public class Board {
 	
 	return position;
 	}
+
 	private Cell getCell(int index) {
     int[] pos = indexToRowCol(index);
     int row = pos[0];
     int col = pos[1];
     return boardCells[row][col];
 	}
- private void setCell(int index, Cell cell){
+
+ 	private void setCell(int index, Cell cell){
 	 int[]pos = indexToRowCol(index);
 	  int row = pos[0];
 	  int col = pos[1];
 	  boardCells[row][col] = cell;
 	}
- public void initializeBoard(ArrayList<Cell> specialCells){ //=--->Door cells @ even indices else the "Rest of cells are @ odd indices".<---=//
-	Cell Door = null;
-	Cell Rest = null;
-	for(int i=0; i<=99;i++){
-		if(i%2 ==0)setCell(i,Door);
-		else{
-			setCell(i,Rest);
-		}
-	}
-	for (int i=0; i<specialCells.size();i++){
-		Cell c = specialCells.get(i);
-		//Pending ..
-		
-	}
-	
-	
-	 
- }
-	private void setCardsByRarity(){ 
 
-	
-	
-	}
-	public static void reloadCards(){
-	 
+ 	public void initializeBoard(ArrayList<Cell> specialCells){ //=--->Door cells @ even indices else the "Rest of cells are @ odd indices".<---=//
+		Cell Door = null;
+		Cell Rest = null;
+		for(int i=0; i<=99;i++){
+			if(i%2 ==0)setCell(i,Door);
+			else{
+				setCell(i,Rest);
+			}
+		}
+		
+		for (int i=0; i<specialCells.size();i++){
+			Cell c = specialCells.get(i);
+			//Pending ..
+			
+		}
 	}
 
 	//public static Card drawCard(){}
-	
-	public Cell[][] getBoardCells() {
-		return boardCells;
-	}
-	
-	public static ArrayList<Monster> getStationedMonsters() {
-		return stationedMonsters;
-	}
-	
-	public static void setStationedMonsters(ArrayList<Monster> stationedMonsters) {
-		Board.stationedMonsters = stationedMonsters;
-	}
-
-    private final Cell[][] boardCells;
-    private static ArrayList<Monster> stationedMonsters;
-    private static ArrayList<Card> originalCards;
-    public static ArrayList<Card> cards;
-
-    public Board(ArrayList<Card> readCards) {
-        this.boardCells = new Cell[Constants.BOARD_ROWS][Constants.BOARD_COLS];
-        Board.stationedMonsters = new ArrayList<>();
-        Board.cards = new ArrayList<>();
-        Board.originalCards = readCards;
-    }
 
     public Cell[][] getBoardCells() {
         return boardCells;
@@ -109,18 +78,6 @@ public class Board {
         Board.stationedMonsters = stationedMonsters;
     }
 
-    public static ArrayList<Card> getOriginalCards() {
-        return originalCards;
-    }
-
-    public static ArrayList<Card> getCards() {
-        return cards;
-    }
-
-    public static void setCards(ArrayList<Card> cards) {
-        Board.cards = cards;
-    }
-
 	public static ArrayList<Card> getOriginalCards() {
 		return originalCards;
 	}
@@ -132,4 +89,8 @@ public class Board {
 	public static void setCards(ArrayList<Card> cards) {
 		Board.cards = cards;
 	}
+
+    public void setBoardCells(Cell[][] boardCells) {
+        this.boardCells = boardCells;
+    }
 }
